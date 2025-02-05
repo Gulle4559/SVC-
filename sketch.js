6b5e16ca8a667ce18edb7fdee4aab3b77e010d0e
Gulle4559 <next16069@edu.nextkbh.dk>
2025-02-05 08:51:09 +0100

    lagt jpg ind

--- sketch.js
+++ sketch.js
@@ -1,4 +1,37 @@
+let o = 0
 let a = 1
 let b = 2
 let c = 3
+let d = 4
+let e = 5
+let f = 6
+let g = 7
+let h = 8
+let i = 9
+
+function preload() {
+o = loadImage("0.jpg")
+a= loadImage("1.jpg")
+b= loadImage("2.jpg")
+c= loadImage("3.jpg")
+d= loadImage("4.jpg")
+e= loadImage("5.jpg")
+f= loadImage("6.jpg")
+g= loadImage("7.jpg")
+h= loadImage("8.jpg")
+i= loadImage("9.jpg")
+
+
+
+
+}
+
+
+if(a ){
+
+
+
+
+
+}
 
