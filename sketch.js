59a1652189e128f8950f1ec8e9fea76524eec5f2
Gulle4559 <next16069@edu.nextkbh.dk>
2025-02-22 08:29:59 +0100

    lavet noget af koden så lommeregneren virker

--- sketch.js
+++ sketch.js
@@ -1,37 +1,49 @@
-let o = 0
-let a = 1
-let b = 2
-let c = 3
-let d = 4
-let e = 5
-let f = 6
-let g = 7
-let h = 8
-let i = 9
+
+
+let o, a, b, c, d, e, f, g, h, i;
+let input = "";
 
 function preload() {
-o = loadImage("0.jpg")
-a= loadImage("1.jpg")
-b= loadImage("2.jpg")
-c= loadImage("3.jpg")
-d= loadImage("4.jpg")
-e= loadImage("5.jpg")
-f= loadImage("6.jpg")
-g= loadImage("7.jpg")
-h= loadImage("8.jpg")
-i= loadImage("9.jpg")
+  o = loadImage("0.jpg");
+  a = loadImage("1.jpg");
+  b = loadImage("2.jpg");
+  c = loadImage("3.jpg");
+  d = loadImage("4.jpg");
+  e = loadImage("5.jpg");
+  f = loadImage("6.jpg");
+  g = loadImage("7.jpg");
+  h = loadImage("8.jpg");
+  i = loadImage("9.jpg");
 
+  images = [o, a, b, c, d, e, f, g, h, i];
+}
 
+function setup() {
+  createCanvas(400, 300);
+  textSize(32);
+  textAlign(CENTER, CENTER);
+}
 
+function draw() {
+  background(220);
+  fill(0);
+  text("Lommeregner", width / 2, 40);
+  displayInput();
+}
 
+function keyPressed() {
+  if (key >= '0' && key <= '9') {
+    input += key;
+  } else if (key === '+' || key === '-') {
+    input += key;
+  } else if (keyCode === ENTER) {
+    calculate();
+  } else if (keyCode === BACKSPACE) {
+    input = input.slice(0, -1);
+  }
 }
 
 
-if(a ){
 
 
-
-
-
-}
 
